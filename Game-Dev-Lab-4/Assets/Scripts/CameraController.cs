92d86781262d389cffb62da78bdb91ad6a77d2a2
callahanhirrel <callahan.hirrel@gmail.com>
2019-04-02 20:26:17 -0500

    beginning of 2d game

---
+++ Game-Dev-Lab-4/Assets/Scripts/CameraController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// References:
+//  - https://answers.unity.com/questions/1006959/stop-camera-when-wall-hit-2d-game.html
+//  - Dr. Karaman's `2DBackgroundScrolling` example
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset;
+
+    public GameObject topEdge;
+    public GameObject bottomEdge;
+    public GameObject leftEdge;
+    public GameObject rightEdge;
+
+    private float xMin;
+    private float xMax;
+    private float yMin;
+    private float yMax;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+        xMin = leftEdge.transform.position.x + 2f;
+        xMax = rightEdge.transform.position.x - 2f;
+        yMin = bottomEdge.transform.position.y + 2f;
+        yMax = topEdge.transform.position.y - 2f;
+    }
+
+    // Use LateUpdate for camera movement
+    void LateUpdate()
+    {
+        float newX = transform.position.x;
+        float newY = transform.position.y;
+
+        // Figure out if camera needs to move on x-axis
+        if (player.transform.position.x > xMin && player.transform.position.x < xMax)
+        {
+            newX = player.transform.position.x + offset.x;
+        }
+
+        // Figure out if camera needs to move on y-axis
+        if (player.transform.position.y > yMin && player.transform.position.y < yMax)
+        {
+            newY = player.transform.position.y + offset.y;
+        }
+
+        // Set new camera position
+        transform.position = new Vector3(newX, newY, transform.position.z);
+    }
+}
---
+++ Game-Dev-Lab-4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 7;
+    private Rigidbody2D rb2d;
+    private Vector2 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float input = Input.GetAxis("Horizontal");
+        velocity = new Vector2(input * speed, rb2d.velocity.y);
+    }
+
+    private void FixedUpdate()
+    {
+        rb2d.MovePosition(rb2d.position + velocity * Time.deltaTime);
+    }
+}
